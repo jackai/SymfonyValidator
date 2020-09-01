0d1570fee2950212f19f93b8be55925e3f89fb38
jackai <kai810425@gmail.com>
2020-09-01 23:18:28 +0800

    WIP: 條件必填欄位

--- RequestValidateListener.php
+++ RequestValidateListener.php
@@ -13,9 +13,12 @@ class RequestValidateListener
     private $emptyStringIsUndefined;
     private $paramRule = [];
     private $queryRule = [];
+    private $ruleRequireParam = [];
+    private $ruleRequireQuery = [];
 
     /**
      * @param RequestEvent $event
+     * @throws \ErrorException
      * @throws \Doctrine\Common\Annotations\AnnotationException
      * @throws \ReflectionException
      * @throws \InvalidArgumentException
@@ -50,6 +53,11 @@ class RequestValidateListener
         $this->checkRequired($annotations->requireQuery, $requset->query->all(), $annotations->requireQueryErrorCode);
 
         foreach ($annotations->param as $k => $v) {
+            if ($v['rule'] == 'require') {
+                array_push($this->ruleRequireParam, $v);
+                continue;
+            }
+
             if (!array_key_exists($v['name'], $this->paramRule)) {
                 $this->paramRule[$v['name']] = [];
             }
@@ -58,12 +66,21 @@ class RequestValidateListener
         }
 
         foreach ($annotations->query as $k => $v) {
+            if ($v['rule'] == 'require') {
+                array_push($this->ruleRequireQuery, $v);
+                continue;
+            }
+
             if (!array_key_exists($v['name'], $this->queryRule)) {
                 $this->queryRule[$v['name']] = [];
             }
 
             array_push($this->queryRule[$v['name']], $v);
         }
+
+        // 檢查條件必填欄位
+        $this->ruleRequireCheck($this->ruleRequireParam, $requset->request->all());
+        $this->ruleRequireCheck($this->ruleRequireQuery, $requset->query->all());
 
         // 填充預設值
         $requset->request->replace($this->fillingData($this->paramRule, $requset->request->all()));
@@ -86,9 +103,126 @@ class RequestValidateListener
     {
         if ($this->throwOnValidateFail) {
             foreach ($requiredColumn as $path) {
-                if (!$this->recursiveValidateRequired($data, explode('.', $path))) {
+                if (!$this->recursiveValidateRequired($data, $path)) {
                     throw new \InvalidArgumentException("$path is required", $errorCode);
                 }
+            }
+        }
+    }
+
+    /**
+     * 驗證特殊必填規則
+     *
+     * @param array $rules
+     * @param array $data
+     *
+     * @throws \ErrorException
+     * @throws \InvalidArgumentException
+     */
+    private function ruleRequireCheck($rules, $data)
+    {
+        foreach ($rules as $rule) {
+            if (!array_key_exists('name', $rule)) {
+                throw new \ErrorException('name is require: ' . json_encode($rule));
+            }
+
+            if (!array_key_exists('ruleOption', $rule)) {
+                throw new \ErrorException('ruleOption is require: ' . json_encode($rule));
+            }
+
+            // 如果目標必填欄位已經填寫了，那就略過
+            if ($this->recursiveValidateRequired($data, $rule['name'])) {
+                continue;
+            }
+
+            $ruleOption = $rule['ruleOption'];
+
+            foreach (['values', 'mode'] as $v) {
+                if (!array_key_exists($v, $ruleOption)) {
+                    throw new \ErrorException(sprintf('Rule missing %s error: %s', $v, json_encode($rule)));
+                }
+            }
+
+            if (!is_array($ruleOption['values']) || count($ruleOption['values']) < 1) {
+                throw new \ErrorException('ruleOption.value error' . json_encode($rule));
+            }
+
+            $errorCode = isset($rule['errorCode']) ? $rule['errorCode'] : null;
+            $errorMsg = isset($rule['errorMsg']) ? $rule['errorMsg'] : "{$rule['name']} is required";
+
+            switch ($ruleOption['mode']) {
+                case 'if':
+                    if (count($ruleOption['values']) < 2) {
+                        throw new \ErrorException('ruleOption.value error' . json_encode($rule));
+                    }
+
+                    $checkValue = $ruleOption['values'];
+                    $targetField = $checkValue[0];
+                    array_shift($checkValue);
+
+                    // 如果條件欄位不存在則略過
+                    if (!$this->recursiveValidateRequired($data, $targetField)) {
+                        continue;
+                    }
+
+                    $fieldValue = $this->getValue($data, $targetField);
+
+                    if(in_array($fieldValue, $checkValue)) {
+                        throw new \InvalidArgumentException($errorMsg, $errorCode);
+                    }
+
+                    break;
+
+                case 'with':
+                    foreach ($ruleOption['values'] as $checkValue) {
+                        if ($this->recursiveValidateRequired($data, $checkValue)) {
+                            throw new \InvalidArgumentException($errorMsg, $errorCode);
+                        }
+                    }
+
+                    break;
+
+                case 'withAll':
+                    $count = 0;
+
+                    foreach ($ruleOption['values'] as $checkValue) {
+                        if ($this->recursiveValidateRequired($data, $checkValue)) {
+                            $count += 1;
+                        }
+                    }
+
+                    if (count($ruleOption['values']) == $count) {
+                        throw new \InvalidArgumentException($errorMsg, $errorCode);
+                    }
+
+                    break;
+
+                case 'without':
+                    foreach ($ruleOption['values'] as $checkValue) {
+                        if (!$this->recursiveValidateRequired($data, $checkValue)) {
+                            throw new \InvalidArgumentException($errorMsg, $errorCode);
+                        }
+                    }
+
+                    break;
+
+                case 'withoutAll':
+                    $count = 0;
+
+                    foreach ($ruleOption['values'] as $checkValue) {
+                        if (!$this->recursiveValidateRequired($data, $checkValue)) {
+                            $count += 1;
+                        }
+                    }
+
+                    if ($count == 0) {
+                        throw new \InvalidArgumentException($errorMsg, $errorCode);
+                    }
+
+                    break;
+
+                default:
+                    throw new \ErrorException('Unknown rule mode: ' . json_encode($rule));
             }
         }
     }
@@ -104,9 +238,8 @@ class RequestValidateListener
     private function fillingData($rules, $data)
     {
         foreach ($rules as $path => $value) {
-            if (!$this->recursiveValidateRequired($data, explode('.', $path)) && isset($value[0]['default'])) {
-                $recursivePath = explode('.', $path);
-                $data = $this->setValue($data, $recursivePath, $value[0]['default']);
+            if (!$this->recursiveValidateRequired($data, $path) && isset($value[0]['default'])) {
+                $data = $this->setValue($data, $path, $value[0]['default']);
             }
         }
 
@@ -158,7 +291,7 @@ class RequestValidateListener
     }
 
     /**
-     * 遞迴驗證必填欄位
+     * 遞迴驗證欄位值是否存在
      *
      * @param $arr
      * @param $recursivePath
@@ -166,6 +299,10 @@ class RequestValidateListener
      */
     private function recursiveValidateRequired($arr, $recursivePath)
     {
+        if (!is_array($recursivePath)) {
+            return $this->recursiveValidateRequired($arr, explode('.', $recursivePath));
+        }
+
         $pathName = $recursivePath[0];
 
         if (count($recursivePath) == 1) {
@@ -190,6 +327,10 @@ class RequestValidateListener
      */
     private function setValue($arr, $recursivePath, $value)
     {
+        if (!is_array($recursivePath)) {
+            return $this->setValue($arr, explode('.', $recursivePath), $value);
+        }
+
         $pathName = $recursivePath[0];
         if (count($recursivePath) == 1) {
             $arr[$pathName] = $value;
@@ -205,5 +346,32 @@ class RequestValidateListener
         $arr[$pathName] = $this->setValue($arr[$pathName], $recursivePath, $value);
 
         return $arr;
+    }
+
+    /**
+     * 遞迴取得資料
+     *
+     * @param $arr
+     * @param $recursivePath
+     * @return mixed
+     */
+    private function getValue($arr, $recursivePath)
+    {
+        if (!is_array($recursivePath)) {
+            return $this->getValue($arr, explode('.', $recursivePath));
+        }
+
+        $pathName = $recursivePath[0];
+
+        if (count($recursivePath) == 1) {
+            return array_key_exists($pathName, $arr) ? $arr[$pathName] : null;
+        }
+
+        if (!array_key_exists($pathName, $arr)) {
+            return null;
+        }
+
+        array_shift($recursivePath);
+        return $this->getValue($arr[$pathName], $recursivePath);
     }
 }
